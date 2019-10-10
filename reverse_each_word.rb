a5a4f8ddd22cbd2a13e38508b43b1a28f4e0faf7
Victoria Hale <vic.hale74@gmail.com>
2019-10-10 13:14:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(words)
   words_array = words.split(" ")
   new_array = []
    new_array = words_array.each do |word|
-     word.revers
+     word.reverse
    
    new_array.join(" ")
   end
